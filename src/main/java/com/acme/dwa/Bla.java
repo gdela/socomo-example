86511bad2c1dfa7215dc02dcb977d78b208d2aec
Wojciech Gdela <wojciech@gdela.pl>
2019-03-04 23:41:10 +0100

    initial commit

---
+++ src/main/java/com/acme/dwa/Bla.java
@@ -0,0 +1,11 @@
+package com.acme.dwa;
+
+import com.acme.raz.Foo;
+
+public class Bla {
+    public Foo foo;
+
+    public static void main(String[] args) {
+        System.out.println("doing bla");
+    }
+}
---
+++ src/main/java/com/acme/raz/Foo.java
@@ -0,0 +1,11 @@
+package com.acme.raz;
+
+import com.acme.dwa.Bla;
+
+public class Foo {
+    public Bla bla;
+
+    public static void main(String[] args) {
+        System.out.println("doing foo");
+    }
+}
