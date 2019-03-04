3ff19226f2cbe6f3f71a470f2cc3ee874623512f
Wojciech Gdela <wojciech@gdela.pl>
2019-03-04 23:58:17 +0100

    added boo

---
+++ src/main/java/com/acme/dwa/Boo.java
@@ -0,0 +1,11 @@
+package com.acme.dwa;
+
+import com.acme.raz.Foo;
+
+public class Boo {
+    public static Foo foo;
+
+    public static void main(String[] args) {
+        System.out.println(foo.bla);
+    }
+}
