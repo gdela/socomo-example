55268fa236c873eac59e6509eb428564ff2e7ad3
Wojciech Gdela <wojciech@gdela.pl>
2019-03-05 00:00:36 +0100

    added bar

---
+++ src/main/java/com/acme/dwa/Bar.java
@@ -0,0 +1,11 @@
+package com.acme.dwa;
+
+import com.acme.raz.Foo;
+
+public class Bar {
+    public static Foo foo;
+
+    public static void main(String[] args) {
+        System.out.println(foo);
+    }
+}
