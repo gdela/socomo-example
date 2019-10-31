556b97eba5877d67e358d2e337ce9f64059ea373
Wojciech Gdela <wojciech@gdela.pl>
2019-10-31 01:04:08 +0100

    The inventory now knows about couriers.

--- src/main/java/pl/gdela/acme/inventory/Inventory.java
+++ src/main/java/pl/gdela/acme/inventory/Inventory.java
@@ -1,5 +1,17 @@
 package pl.gdela.acme.inventory;
 
+import pl.gdela.acme.couriers.Carrier;
+import pl.gdela.acme.couriers.dhl.DhlCarrier;
+import pl.gdela.acme.couriers.ups.UpsCarrier;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class Inventory {
+
+    public List<Carrier> availableCarriers() {
+        return asList(new UpsCarrier(), new DhlCarrier());
+    }
 
 }
